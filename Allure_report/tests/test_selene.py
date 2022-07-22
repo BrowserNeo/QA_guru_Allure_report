9ff0a956b9d9774d881b3e1d13f3be8060711938
George BrowserNeo <disconnect>
2022-07-23 00:13:12 +0300

    рабочий вариант теста селен

--- Allure_report/tests/test_selene.py
+++ Allure_report/tests/test_selene.py
@@ -7,11 +7,11 @@ def test_github():
     browser.open('https://github.com')
 
 
-    s('.header-search-input').click()
-    s('.header-search-input').send_keys("eroshenkoam/allure-example")
-    s('.header-search-input').submit()
+    browser.element('.header-search-input').click()
+    browser.element('.header-search-input').send_keys("eroshenkoam/allure-example")
+    browser.element('.header-search-input').submit()
 
-    s(by.link_text(eroshenko/allure-example)).click()
+    browser.element(by.link_text("eroshenkoam/allure-example")).click()
 
-    s("#issue-tab").click()
-    s(by.partial_text("#76")).should(be.visible)
+    browser.element("#issues-tab").click()
+    browser.element(by.partial_text("#76")).should(be.visible)
