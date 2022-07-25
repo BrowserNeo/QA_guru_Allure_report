07b19fbc97cf896158a90b9236ca6669efbeb9c4
George BrowserNeo <disconnect>
2022-07-26 01:28:38 +0300

    new test labels

---
+++ Allure_report/tests/test_labels.py
@@ -0,0 +1,25 @@
+import allure
+from allure_commons.types import Severity
+
+
+def test_no_labels():
+    pass
+
+
+def test_dynamic_labels():
+    allure.dynamic.tag("web")
+    allure.dynamic.severity(Severity.BLOCKER)
+    allure.dynamic.feature("Задачи в репозитории")
+    allure.dynamic.story("Неавторизованный пользователь не может создать задачу в репозитории")
+    allure.dynamic.link("https://github.com", name="Testing")
+    pass
+
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "eroshenkoam")
+@allure.feature("Задачи в репозитории")
+@allure.story("Авторизованный пользователь может создать задачу в репозитории")
+@allure.link("https://github.com", name="Testing")
+def test_decorator_labels():
+    pass
